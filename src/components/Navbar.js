2b2c29cf6e2f11a6f39270c1f82ccf2cbfc88f1e
ranaji038 <mailforranaji@gmail.com>
2022-10-15 11:32:19 +0530

    Added Dark Mode Functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,6 +39,25 @@ export default function Navbar(props) {
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-primary" type="submit">Search</button>
       </form> */}
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              onClick={props.toggleMode}
+              id="flexSwitchCheckDefault"
+            />
+            <label
+              className="form-check-label"
+              htmlFor="flexSwitchCheckDefault"
+            >
+              Enable DarkMode
+            </label>
+          </div>
         </div>
       </div>
     </nav>
