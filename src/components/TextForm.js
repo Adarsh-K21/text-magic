c445bf2abddb4edc7e9c1980b63a182013e630f7
hijal <hijikeshhijal@gmail.com>
2022-10-17 20:45:00 +0600

    fix navbar toggle mode, footer class name, textForm class name

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -105,7 +105,7 @@ export default function TextForm(props) {
 
         <button disabled={text.length === 0} className='btn btn-primary mx-2 my-1' onClick={handleClear}>Clear Text</button>
         <button disabled={text.length === 0} className='btn btn-primary mx-2 my-1' onClick={handleDownload}>Download as Text</button>
-        <input class="rounded mx-2 my-1 border border-secondary" type="search" placeholder="Search" aria-label="Search" style={{height:"38px"}} onChange={searchChange}/>
+        <input className="rounded mx-2 my-1 border border-secondary" type="search" placeholder="Search" aria-label="Search" style={{height:"38px"}} onChange={searchChange}/>
 
 
       </div>
